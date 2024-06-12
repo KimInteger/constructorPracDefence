5de6fa693aa28781d1a361aaf140d84e23205749
KimInteger <kjs20151240@gmail.com>
2024-06-12 20:51:49 +0900

    테이블 생성

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('app.js');
+const sqlite3 = require('sqlite3').verbose();
+
+const db = new sqlite3.Database('./database/balloonDefence.db');
+
+db.run("CREATE TABLE IF NOT EXISTS monster (id INTEGER PRAIMARY KEY, HP INTEGER NOT NULL, DEF INTEGER NOT NULL, SPEED INTEGER NOT NULL)",(err)=>{
+  if(err) {
+    throw new Error('테이블이 생성되지 아니함');
+  } else {
+    console.log(1);
+  }
+});
+
+
+db.close();
